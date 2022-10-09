6d5334fb5028534c57c92226a31a883c01f1e77c
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-10-10 04:35:08 +0900

    실습 4 - 멜론뷰 구현

--- Seminar/2nd_Seminar/2nd_Seminar/SceneDelegate.swift
+++ Seminar/2nd_Seminar/2nd_Seminar/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let window = UIWindow(windowScene: windowScene)
             window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
                     
-            let rootVC = ThirdViewController()
+            let rootVC = MelonViewController()
                     
             window.rootViewController = rootVC
             window.makeKeyAndVisible()
