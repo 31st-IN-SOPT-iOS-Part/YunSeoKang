455a097ec7b49227a45036b58719fa877f4a4f7b
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-10-11 04:26:35 +0900

    [Add] #4 - 이미지 리터럴화

---
+++ Assignment/Kakako-Clone/Kakako-Clone/Global/Literals/ImageLiterals.swift
@@ -0,0 +1,20 @@
+//
+//  ImageLiterals.swift
+//  Kakako-Clone
+//
+//  Created by 강윤서 on 2022/10/11.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct FriendTab {
+        static let profileImage = UIImage(named: "profileImage")
+        static let settingButton = UIImage(named: "settingsButton")
+        static let closeButton = UIImage(named: "profileCloseBtn")
+        static let editButton = UIImage(named: "profileEditImg")
+        static let storyButton = UIImage(named: "profileStoryImg")
+        static let talkButton = UIImage(named: "profileTalkImg")
+    }
+}
