68e0e38115e78ead9e7359e3e5ac62935da2914f
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-10-15 14:23:33 +0900

    [Chore] 프로젝트 세팅

---
+++ Seminar/3rd_Seminar/3rd_Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3rd_Seminar
+//
+//  Created by 강윤서 on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
